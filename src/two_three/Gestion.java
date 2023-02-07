266d44a9482ac0b1ba294392cfd7bc7b68079dbe
JourdainK <K.jourdain@condorcet.be>
2023-02-07 17:40:47 +0100

    menu -> TODO Client class in Gestion

---
+++ src/two_three/Gestion.java
@@ -0,0 +1,92 @@
+package two_three;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Gestion {
+    private List<Client> clientsList;
+    private List<Taxi> taxisList;
+    private List<Adresse> adressesList;
+    private List<Location> locationsList;
+    private Scanner sc = new Scanner(System.in);
+
+    public Gestion(){
+
+        clientsList = new ArrayList<>();
+        taxisList = new ArrayList<>();
+        adressesList = new ArrayList<>();
+        locationsList = new ArrayList<>();
+        //TODO testing classes > for all classes declare instances to test each classes
+        menu();
+
+    }
+
+    public void menu(){
+        String option = "\n1 - Menu client\n2 - Menu Taxi\n3 - Menu Adresses\n4 - Menu Locations\n5 - Quitter";
+        String choiceMenu1;
+        int choiceMenu;
+
+        System.out.println(" -- Taxi service --");
+
+        do{
+            do{
+                System.out.println(option);
+                System.out.println("Votre choix : ");
+                choiceMenu1 = saisie("[1-5]","Veuillez saisir un nombre compris entre 1 et 5.\nVotre choix : ");
+                choiceMenu = Integer.parseInt(choiceMenu1);
+            }while(choiceMenu < 1 || choiceMenu > 5);
+
+            switch(choiceMenu){
+                //TODO develop method for these
+                case 1 -> menuClient();
+                case 2 -> System.out.println("Menu Taxi");
+                case 3 -> System.out.println("Menu Addresses");
+                case 4 -> System.out.println("Menu Locations");
+                case 5 -> System.out.println("Au revoir");
+                default -> System.out.println("Erreur de saisie.\n");
+            }
+        }while(choiceMenu!=5);
+
+    }
+
+    public void menuClient(){
+        String optionClient = "\n1 - Enregistrer un client\n2 - Voir les locations d'un client\n3 - Voir les informations d'un client\n4 - Retour au menu précédent";
+        String choiceMenuCli1;
+        int choiceMenuCli;
+
+        System.out.println("-- Menu Client --");
+        do{
+            do{
+                System.out.println(optionClient);
+                System.out.println("Votre choix : ");
+                choiceMenuCli1 = saisie("[1-4]","Veuillez saisir un nombre compris entre 1 et 4\nVotre choix : ");
+                choiceMenuCli = Integer.parseInt(choiceMenuCli1);
+            }while(choiceMenuCli < 1 || choiceMenuCli > 4);
+
+            switch (choiceMenuCli){
+                //TODO develop methods for these
+                case 1 -> System.out.println("Enregistrer un client");
+                case 2 -> System.out.println("Voir les locations d'un client");
+                case 3 -> System.out.println("Voir les infos d'un client");
+                default -> System.out.println("Erreur de saisie\n");
+            }
+        }while(choiceMenuCli!=4);
+    }
+
+
+    public String saisie(String regex, String message) {
+        Scanner sc = new Scanner(System.in);
+        boolean check = false;
+        String phrase;
+        do {
+            phrase = sc.nextLine();
+            if (phrase.matches(regex)) {
+                check = true;
+            } else {
+                System.out.println(message);
+            }
+        } while (!check);
+        return phrase;
+    }
+}
